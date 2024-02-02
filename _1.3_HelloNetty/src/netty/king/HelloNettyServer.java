318f00831bf80ef3c9494af9db9975efc0170546
qq4215279 <liu865488158@qq.com>
2024-02-02 18:21:16 +0800

    rename

--- _1.3_HelloNetty/src/netty/king/HelloNettyServer.java
+++ _1.3_HelloNetty/src/netty/king/HelloNettyServer.java
@@ -10,6 +10,27 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class HelloNettyServer {
+
+    /**
+     * netty-4.1.48.Final 源码
+     *  1. buffer [netty-buffer]  ->  io.netty.buffer
+     *  2. codec [netty-codec]  ->  io.netty.handler.codec
+     *  3. common [netty-common]  ->  io.netty.util
+     *  4. handler [netty-handler]  ->    io.netty.handler
+     *  5. resolver [netty-resolver]  ->  io.netty.resolver
+     *  6. transport [netty-transport]  ->  io.netty.bootstrap  &&  io.netty.channel
+     *
+     * netty-4.1.48.Final.jar
+     * io.netty.bootstrap  ->      transport [netty-transport]
+     * io.netty.buffer     ->      buffer [netty-buffer]
+     * io.netty.channel    ->      transport [netty-transport]
+     * io.netty.handler    ->      handler [netty-handler]
+     * io.netty.internal   ->
+     * io.netty.resolver   ->      resolver [netty-resolver]
+     * io.netty.util       ->      common [netty-common]
+     */
+
+
     private int port;
 
     /**
@@ -22,12 +43,12 @@ public class HelloNettyServer {
     /**
      * @throws Exception
      */
-    public void run() throws Exception{
+    public void run() throws Exception {
         EventLoopGroup boosGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap b = new ServerBootstrap();
-            b.group(boosGroup,workerGroup)
+            b.group(boosGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
@@ -35,7 +56,7 @@ public class HelloNettyServer {
                             socketChannel.pipeline().addLast(new HelloNettyServerHandler());
                         }
                     })
-                    .option(ChannelOption.SO_BACKLOG,128)
+                    .option(ChannelOption.SO_BACKLOG, 128)
                     .childOption(ChannelOption.SO_KEEPALIVE, true);
             ChannelFuture f = b.bind(port).sync();
             f.channel().closeFuture().sync();
@@ -52,7 +73,7 @@ public class HelloNettyServer {
     public static void main(String[] args) throws Exception {
         System.out.println("开始建立Netty服务器...");
         int port = 8080;
-        if(args.length > 0 ){
+        if (args.length > 0) {
             port = Integer.parseInt(args[0]);
         }
         new HelloNettyServer(port).run();
