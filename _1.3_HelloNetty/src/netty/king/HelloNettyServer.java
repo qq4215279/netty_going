c6a185202fe728625e20efe554e6cdaf45be965b
qq4215279 <liu865488158@qq.com>
2024-02-07 10:12:57 +0800

    going

--- _1.3_HelloNetty/src/netty/king/HelloNettyServer.java
+++ _1.3_HelloNetty/src/netty/king/HelloNettyServer.java
@@ -1,13 +1,54 @@
 package netty.king;
 
+import io.netty.bootstrap.AbstractBootstrap;
+import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.AbstractByteBufAllocator;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.ByteBufHolder;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.CompositeByteBuf;
+import io.netty.buffer.DefaultByteBufHolder;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.buffer.Unpooled;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelConfig;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelOutboundHandler;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.CombinedChannelDuplexHandler;
+import io.netty.channel.DefaultChannelPipeline;
+import io.netty.channel.EventLoop;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.channel.local.LocalChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.ByteToMessageCodec;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.MessageToByteEncoder;
+import io.netty.handler.codec.MessageToMessageCodec;
+import io.netty.handler.codec.MessageToMessageDecoder;
+import io.netty.handler.codec.MessageToMessageEncoder;
+import io.netty.handler.codec.ReplayingDecoder;
+import io.netty.util.ByteProcessor;
+import io.netty.util.ReferenceCountUtil;
+import io.netty.util.ReferenceCounted;
+import io.netty.util.concurrent.DefaultEventExecutorGroup;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+import java.nio.channels.FileChannel;
 
 public class HelloNettyServer {
 
@@ -28,7 +69,35 @@ public class HelloNettyServer {
      * io.netty.internal   ->
      * io.netty.resolver   ->      resolver [netty-resolver]
      * io.netty.util       ->      common [netty-common]
+     *
+     * 3：
+     * Handle
+     *
+     * 4：
+     * ByteBuf ByteBufHolder(DefaultByteBufHolder) ByteBufAllocator(AbstractByteBufAllocator UnpooledByteBufAllocator PooledByteBufAllocator)
+     * CompositeByteBuf ByteBufUtil
+     * AbstractByteBuf Unpooled  ReferenceCounted  FileChannel
+     *
+     * 5:
+     * Channel LocalChannel  EmbeddedChannel  ChannelPipeline  ChannelConfig
+     *
+     * 6:
+     * Channel: ChannelPipeline(DefaultChannelPipeline)  ChannelFuture  ChannelPromise  EventLoop  ByteBuf
+     * ChannelHandler(ChannelInboundHandler  ChannelOutboundHandler  ChannelHandlerAdapter(ChannelInboundHandlerAdapter ChannelOutboundHandlerAdapter))
+     * ChannelHandlerContext
+     * EventLoopGroup  EventExecutorGroup(DefaultEventExecutorGroup)
+     *
+     * 7:
+     * ReferenceCountUtil  ChannelOutboundHandler  MessageToByteEncoder  MessageToMessageEncoder
+     * ChannelInboundHandler  ByteToMessageDecoder  ReplayingDecoder  MessageToMessageDecoder
+     * ByteToMessageCodec  MessageToMessageCodec  CombinedChannelDuplexHandler
+     *
+     * 8.
+     * AbstractBootstrap(Bootstrap  ServerBootstrap)
+     * ChannelOption
+     *
      */
+
 
 
     private int port;
@@ -44,6 +113,9 @@ public class HelloNettyServer {
      * @throws Exception
      */
     public void run() throws Exception {
+        //
+
+
         EventLoopGroup boosGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
